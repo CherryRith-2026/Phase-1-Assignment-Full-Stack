b849326f0ba01ffdbdb3e63e0e6028a92b681429
Cheri <cheri@Cheris-MacBook-Pro-2.local>
2026-08-26 02:50:37 +1000

    Connect Angular login to Express backend

--- client/src/app/login/login.ts
+++ client/src/app/login/login.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
+
 export class Login {
+
+  username = '';
+  password = '';
+  errorMessage = '';
+
+  constructor(
+    private http: HttpClient,
+    private router: Router
+  ) {}
+
+  login() {
+
+    this.http.post<any>('http://localhost:3000/api/login', {
+      username: this.username,
+      password: this.password
+    }).subscribe(response => {
+
+      if (response.success) {
+        this.router.navigate(['/home']);
+      } else {
+        this.errorMessage = response.message;
+      }
+
+    });
+
+  }
 
 }
--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(cors());
 
 //user API
 const users = [
